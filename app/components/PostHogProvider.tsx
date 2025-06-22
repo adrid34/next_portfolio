4c86155f942bca1d25667bd2d498e299d9100ae2
Adrian Madrid <adrianmadrid5245@gmail.com>
2025-06-22 12:50:58 -0500

    quick fix

--- app/components/PostHogProvider.tsx
+++ app/components/PostHogProvider.tsx
@@ -3,12 +3,8 @@
 import posthog from "posthog-js"
 import { PostHogProvider as PHProvider } from "posthog-js/react"
 import { useEffect } from "react"
-import { usePathname, useSearchParams } from "next/navigation"
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: "/ingest",
